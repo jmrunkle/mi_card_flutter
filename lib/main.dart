8f1fd6fe914fcbf3f6d62b9513558a7fc594f8f5
Jason <jmrunkle@gmail.com>
2020-04-27 19:11:34 -0700

    Finish the Mi Card App.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('images/jason.jpg'),
+                radius: 50.0,
+              ),
+              Text(
+                'Jason Runkle',
+                style: GoogleFonts.pacifico(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'SOFTWARE ENGINEER',
+                style: GoogleFonts.sourceSansPro(
+                  color: Colors.white.withOpacity(0.5),
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                    size: 25.0,
+                  ),
+                  title: Text(
+                    '(555) 555-5555',
+                    style: GoogleFonts.sourceSansPro(
+                      color: Colors.teal,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'nobody@gmail.com',
+                    style: GoogleFonts.sourceSansPro(
+                      color: Colors.teal,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
